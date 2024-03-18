d8895323c2ab12e0b6dabc3727f7986b9bca7d22
Aleksandr Hemingway <deepspaceghost@protonmail.com>
2024-03-18 12:00:07 -0400

    Update debug.py

--- code/debug.py
+++ code/debug.py
@@ -1,13 +1,12 @@
-import pygame
+import core as c
 
-pygame.init()
-font = pygame.font.Font(None, 30)
+c.pygame.init()
+font = c.Font(None, 30)
 
+def debug(info, x=10, y=10):
 
-def debug(info, y=10, x=10):
-
-    display_surface = pygame.display.get_surface()
-    debug_surf = font.render(str(info), True, 'White')
-    debug_rect = debug_surf.get_rect(topleft=(x, y))
-    pygame.draw.rect(display_surface, 'Black', debug_rect)
-    display_surface.blit(debug_surf, debug_rect)
+    surface = c.get_surface()
+    debug_surface = font.render(str(info), True, "white")
+    debug_rectangle = debug_surface.get_rect(topleft=(x, y))
+    c.rect(surface, "black", debug_rectangle)
+    surface.blit(debug_surface, debug_rectangle)
